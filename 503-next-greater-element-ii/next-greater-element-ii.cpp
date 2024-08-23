670dfd7a676f66a58591a395cb265b842407a27c
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2024-08-23 19:58:44 +0530

    Time: 28 ms (23.10%) | Memory: 27.3 MB (20.32%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -3,31 +3,54 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         vector<int>ans;
         ans = nums;
-        for(int i=0;i<nums.size();i++){
-            ans.push_back(nums[i]);
-        }
-        map<int,int>mpp;
+        // for(int i=0;i<nums.size();i++){
+        //     ans.push_back(nums[i]);
+        // }
         stack<int>st;
         for(int i=ans.size()-1;i>=0;i--){
             if(st.empty()){
                 st.push(ans[i]);
-                if(i<nums.size()){
-                    nums[i]=-1;
+                // if(i<nums.size()){
+                //     nums[i]=-1;
+                // }
+            }
+            else{
+                while(!st.empty() and st.top()<=ans[i]){
+                    st.pop();
                 }
+                // if(st.empty()){
+                    // if(i<nums.size()){
+                    // nums[i]=-1;
+                    // }
+                // }
+                // else{
+                    // if(i<nums.size()){
+                    // nums[i]=st.top();
+                    // }
+                // }
+                st.push(ans[i]);
+            }
+        }
+         for(int i=ans.size()-1;i>=0;i--){
+            if(st.empty()){
+                st.push(ans[i]);
+                // if(i<nums.size()){
+                    nums[i]=-1;
+                // }
             }
             else{
                 while(!st.empty() and st.top()<=ans[i]){
                     st.pop();
                 }
                 if(st.empty()){
-                    if(i<nums.size()){
+                    // if(i<nums.size()){
                     nums[i]=-1;
-                    }
+                    // }
                 }
                 else{
-                    if(i<nums.size()){
+                    // if(i<nums.size()){
                     nums[i]=st.top();
-                    }
+                    // }
                 }
                 st.push(ans[i]);
             }

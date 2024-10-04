dde081800f7cdbbee95a306d1e735061bfae9eb6
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2024-10-04 22:02:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (73.02%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double binpow(double a, int b) {
+    if (b == 0) return 1;
+    double res = binpow(a, b / 2);
+    if (b % 2) return res * res * a;
+    else return res * res;
+    }
+    double myPow(double x, int n) {
+        if(n<0){
+            return 1/binpow(x,n);
+        }
+        return binpow(x,n);
+    }
+};

8dca781b737fe8e5513f2368ecba17a67ec36b66
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2024-11-14 21:29:10 +0530

    Time: 27 ms (85.77%) | Memory: 87.3 MB (72.13%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+     bool solve(int n, vector<int>& quantities, int item)
+    {
+        if(item == 0) return false;
+        int ss = 0;
+        for(int i=0;i<quantities.size();i++)
+        {
+            ss += (quantities[i] - 1) / item + 1;
+            if(ss > n) return false;
+        }
+        return true;
+    }
+    int minimizedMaximum(int n, vector<int>& quantities) {
+         int low = 1;
+        int high = *max_element(quantities.begin(), quantities.end());
+        int ans = -1;
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            if(solve(n, quantities, mid))
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};

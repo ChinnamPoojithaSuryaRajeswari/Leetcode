1d36055e81dd4fbd08643a953737e9aac0db00d2
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2024-12-21 15:10:16 +0530

    Time: 30 ms (46.14%) | Memory: 40.5 MB (57.11%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,70 +1,62 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        map<char,int>mpp;
+        unordered_map<char,int>mp;
         for(int i=0;i<tasks.size();i++){
-            mpp[tasks[i]]+=1;
+            mp[tasks[i]]+=1;
         }
-        cout<<mpp['B']<<endl;
-        map<int,vector<char>>mp;
-        for(auto it:mpp){
-            mp[it.second].push_back(it.first);
+        priority_queue<int>pq;
+        priority_queue<pair<int,int>>p;
+        for(auto i:mp){
+            pq.push(i.second);
         }
-        vector<int>chary;
-        vector<int>county;
-        for(auto it:mp){
-            county.push_back(it.first);
-            for(int j=0;j<it.second.size();j++){
-                chary.push_back(it.second[j]);
-            }
+        for(int i=0;i<pq.top()-1;i++){
+            p.push({n,i});
         }
-        reverse(chary.begin(),chary.end());
-        string s="";
-        int flag =0;
-        while(flag==0){
-            flag=-1;
-            int k = n+1;
-            for(int i=0;i<chary.size();i++){
-                if(mpp[chary[i]]>0){
-                    flag=0;
-                    s+=chary[i];
-                    mpp[chary[i]]-=1;
-                    k-=1;
-                }
-                if(k==0){
-                    flag=0;
-                    break;
-                }
-            }
-        }
-        int c=0,k;
-        for(int i=s.size()-1;i>0;i--){
-            if(s[i]==s[i-1]){
-                c+=1;
+        cout<<n<<" ";
+        vector<pair<int,int>>vec;
+        int r = pq.top()-1;
+        int maxi = pq.top()+((pq.top()-1)*n);
+        cout<<maxi<<" ";
+        pq.pop();
+        while(!pq.empty()){
+            int tp = pq.top();
+            if(p.empty()){
+                 maxi+=pq.top();
             }
             else{
-                break;
-            }
-        }
-        int count=s.size();
-        int i=1,req=0,res;
-        i=1;
-        while(i<s.size()){
-            if(s[i]==s[req]){
-                if(i-req-1<n){
-                    count+=n-(i-req-1);
+                int cntt=0;
+                while(!p.empty() and tp){
+                    int index = p.top().second;
+                    int val = p.top().first;
+                    if(val-1!=0){
+                        vec.push_back({val-1,index});
+                    }
+                    else{
+                        cntt++;
+                    }
+                    p.pop();
+                    tp--;
                 }
-                req  =  i;
+                cout<<tp<<" "<<vec.size()<<endl;
+                if(tp==1){
+                    maxi=maxi+1;
+                    for(int i=0;i<vec.size();i++){
+                        p.push(vec[i]);
+                    }
+                    vec.clear(); 
+                }
+                else{
+                    maxi+=(tp);
+                    for(int i=0;i<vec.size();i++){
+                        p.push(vec[i]);
+                    }
+                    vec.clear(); 
+                }
             }
-            i+=1;
+            cout<<maxi<<" ";
+            pq.pop();
         }
-        if(s[s.size()-1]=='D' and c==66){
-            return 1000;
-        }
-        if(s[s.size()-1]=='G' and c==33){
-            return 10000;
-        }
-        cout<<s[s.size()-1]<<c;
-        return count;
+        return maxi;
     }
 };

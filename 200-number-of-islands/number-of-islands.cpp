532af57788a57afca1126ded8bb5ee4701e8ef33
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2025-05-13 13:46:00 +0530

    Time: 26 ms (64.55%) | Memory: 17.3 MB (50.98%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void call(vector<vector<char>>&grid,vector<vector<int>>&visited,int i,int j){
+        if(i>=grid.size() or i<0 or j>=grid[0].size() or j<0)return;
+        if(visited[i][j]==1 or grid[i][j]=='0')return;
+        visited[i][j] = 1;
+        call(grid,visited,i+1,j);
+        call(grid,visited,i-1,j);
+        call(grid,visited,i,j+1);
+        call(grid,visited,i,j-1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        vector<vector<int>>visited(grid.size(),vector<int>(grid[0].size(),0));
+        int ans=0;
+        for(int i = 0 ; i < grid.size() ; i++){
+            for(int  j = 0 ; j < grid[i].size() ; j++){
+                if(visited[i][j]==0 and grid[i][j]=='1'){
+                    call(grid,visited,i,j);
+                    ans+=1;
+                }
+            }
+        }
+        return ans;
+    }
+};

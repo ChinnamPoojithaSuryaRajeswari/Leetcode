6bad67d5c37dc2af814634c5e4a9197f55dad591
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2025-05-13 17:15:37 +0530

    Time: 19 ms (37.69%) | Memory: 23.1 MB (27.22%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int call(vector<vector<int>>& matrix, vector<vector<int>>& visited, int i, int j, int prev) {
+        if(i >= matrix.size() || i < 0 || j >= matrix[0].size() || j < 0) return 0;
+        if(matrix[i][j] <= prev) return 0;
+        if(visited[i][j] != 0) return visited[i][j];
+        int left = call(matrix, visited, i, j - 1, matrix[i][j]);
+        int right = call(matrix, visited, i, j + 1, matrix[i][j]);
+        int up = call(matrix, visited, i - 1, j, matrix[i][j]);
+        int down = call(matrix, visited, i + 1, j, matrix[i][j]);
+        visited[i][j] = 1 + max({left, right, up, down});
+        return visited[i][j];
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        vector<vector<int>>visited(matrix.size(),vector<int>(matrix[0].size(),0));
+        for(int i = 0 ; i < matrix.size() ; i++){
+            for(int j = 0 ; j < matrix[0].size() ; j++){
+                visited[i][j] = call(matrix,visited,i,j,INT_MIN);
+            }
+        }
+        int ans = 0 ;
+        for(int i=0;i<visited.size();i++){
+            for(int j=0;j<visited[0].size();j++){
+                ans = max(ans,visited[i][j]);
+            }
+        }
+        return ans==0 ? 1:ans;
+    }
+};

a6eb2cb699f6bf684b30697562af042ba9accbaa
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2025-07-31 21:23:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (90.95%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= s.size(); i++) {
+            for (auto  w:wordDict) {
+                int start = i - w.size();
+                if (start >= 0 && dp[start] && s.substr(start, w.size()) == w) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()]; 
+    }
+};

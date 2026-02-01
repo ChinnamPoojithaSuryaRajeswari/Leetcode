48b1d6717b4b945c9b52111516baa01d63aabfdd
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2026-02-01 10:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (88.79%) - LeetSync

---
+++ 4194-count-monobit-integers/count-monobit-integers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countMonobit(int n) {
+        if(n==0)return 1;
+        if(n==1)return 2;
+        int ans = 2;
+        for(int i=2;i<=n;i++){
+            if((i&(i+1))==0)ans++;
+        }
+        return ans;
+    }
+};

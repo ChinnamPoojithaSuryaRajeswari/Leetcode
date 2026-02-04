5d0152e310dbc2dd813fc065fd3563a423ca11bd
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2026-02-04 11:37:47 +0530

    Time: 754 ms (33.74%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary FROM employee e JOIN department d ON e.departmentId = d.id JOIN (SELECT departmentId, MAX(salary) AS max_salary FROM employee GROUP BY departmentId) m ON e.departmentId = m.departmentId AND e.salary = m.max_salary;

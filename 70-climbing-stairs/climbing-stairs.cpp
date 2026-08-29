4b6ad54d7da3eb31b76f23af4fc5b52746692420
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2026-08-29 11:12:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    int fibo(int n,vector<int>&dp){
-        if(n<=1) return n;
-        if(dp[n]!=-1)return dp[n];
-        dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
-        return dp[n];
-    }
     int climbStairs(int n) {
-        vector<int>dp(n+2,-1);
-        fibo(n+1,dp);
-        return dp[n+1];
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
